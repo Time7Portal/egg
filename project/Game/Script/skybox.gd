672affaa934f751104752c94a948cf05cf6ff50e
Time7Portal <wjdals930315@gmail.com>
2024-10-13 18:19:50 +0900

    스카이박스, 바닥 잔디 재질

---
+++ project/Game/Script/skybox.gd
@@ -0,0 +1,12 @@
+extends WorldEnvironment
+
+@export var sky_rotation_speed = 0.2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$".".environment.sky_rotation.y += delta * sky_rotation_speed
