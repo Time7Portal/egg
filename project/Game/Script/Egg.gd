102a4c8a6bcd3def0b997c82c5d5ce9b1dd30d65
Time7Portal <wjdals930315@gmail.com>
2024-12-08 21:43:44 +0900

    updated egg colors (bugfix)

--- project/Game/Script/Egg.gd
+++ project/Game/Script/Egg.gd
@@ -18,27 +18,34 @@ enum Grade{
 	RAINBOW, 
 	COUNT
 }
-@export var _grade = Grade.BRONZE;
-
-
-func _ready() -> void:
-	# Grade 에 따른 달걀 색상 적용
-	var material = egg.get_active_material(0);
-	
-	match _grade:
-		Grade.BRONZE:
-			material.albedo_color = Color(0.953, 0.796, 0.652);
-		Grade.SILVER:
-			material.albedo_color = Color(1, 1, 1);
-		Grade.GOLD:
-			material.albedo_color = Color(1, 1, 0);
-		Grade.PLATINUM:
-			material.albedo_color = Color(0.299, 0.353, 0.372);
-		Grade.DIAMOND:
-			material.albedo_color = Color(0.566, 0.732, 0.89);
-		Grade.RAINBOW:
-			material.albedo_color = Color(1, 0.5, 1);
+@export var _grade = Grade.BRONZE:
+	set(new_grade):
+		_grade = new_grade;
+		#print("LV." + str(_grade) + " egg spawned!")
 		
+		# Grade 에 따른 달걀 색상 적용
+		var material = egg.get_active_material(0).duplicate();
+		
+		match _grade:
+			Grade.BRONZE:
+				material.albedo_color = Color(0.953, 0.796, 0.652);
+				egg.set_surface_override_material(0, material)
+			Grade.SILVER:
+				material.albedo_color = Color(1, 1, 1);
+				egg.set_surface_override_material(0, material)
+			Grade.GOLD:
+				material.albedo_color = Color(1, 1, 0);
+				egg.set_surface_override_material(0, material)
+			Grade.PLATINUM:
+				material.albedo_color = Color(0.299, 0.353, 0.372);
+				egg.set_surface_override_material(0, material)
+			Grade.DIAMOND:
+				material.albedo_color = Color(0.566, 0.732, 0.89);
+				egg.set_surface_override_material(0, material)
+			Grade.RAINBOW:
+				material.albedo_color = Color(1, 0.5, 1);
+				egg.set_surface_override_material(0, material)
+
 
 func initializeStatus(hatchTime:float) -> void:
 	_status = Status.new();
