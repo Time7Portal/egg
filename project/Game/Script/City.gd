d09c3caf412d88c981c54b98e2e0747a031ab8fb
Time7Portal <wjdals930315@gmail.com>
2025-01-19 18:32:04 +0900

    house, poop2 모델 추가 및 버그수정

--- project/Game/Script/City.gd
+++ project/Game/Script/City.gd
@@ -3,11 +3,10 @@ class_name City;
 
 @export var _storeUI:Control;
 
-func _on_gui_input(event: InputEvent) -> void:	
-	if(event.is_action_pressed("Click") == false):
-		return;
+# 도시 클릭하면 상점 표시
+func _on_gui_input(event: InputEvent) -> void:
+	if(event.is_action_pressed("Click") == true):
+		print("city clicked!")
+		_storeUI.visible = true;
 		
-	if(Manager.targetSceneIndex != 1):
-		return;
-		
-	_storeUI.visible = true;
+	
--- project/Game/UI/store.gd
+++ project/Game/UI/store.gd
@@ -11,11 +11,11 @@ func _process(delta: float) -> void:
 	pass
 
 
-# 돌아가기 버튼을 누르면 창 지우기
+# 돌아가기 버튼을 누르면 창 감추기
 func _on_exit_button_pressed() -> void:
-	queue_free();
+	visible = false;
 
 
 # 구매 버튼을 누르면 아이템 구매
 func _on_buy_button_pressed() -> void:
-	pass # Replace with function body.
+	pass
